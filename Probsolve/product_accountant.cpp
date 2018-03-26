22c869c9a6c10dcd048958be2370a21f3174cfc2
biggywk <big.baby-@hotmail.com>
2018-03-26 13:21:23 +0700

    create simple warehouse

---
+++ Probsolve/product_accountant.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int N_order;
+    cin>>N_order;
+    int order,tmp;
+    for(int i=0;i<N_order;i++)
+    {
+        cin>>order>>tmp;
+        if(order==1)
+        {
+
+        }
+        else if(order==2)
+        {
+
+        }
+        else if(order==3)
+        {
+
+        }
+    }
+
+
+return 0;}
