b3b09af7c58add3d127ec9b8d5c34fc4fc50a096
biggywk <big.baby-@hotmail.com>
2018-03-27 12:34:33 +0700

    update midwarehouse

--- Probsolve/midwarehouse.cpp
+++ Probsolve/midwarehouse.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<list>
 using namespace std;
 int main()
 {
@@ -6,8 +7,36 @@ int main()
     cin>>n>>m>>k;
     int arr[n];
     int tmp;
-    for(int i=0;i<m+k;i++)
+    int tmp1;
+    list<int>node[n+1];
+    list<int>::iterator it;
+    list<int>::iterator it1;
+    int can=0;
+    for(int i=0;i<m;i++)
     {
-        cin<<tmp
+        cin>>tmp>>tmp1;
+
+        node[tmp].push_back(tmp1);
+        node[tmp1].push_back(tmp);
+
     }
+
+    for(int i=0;i<k;i++)
+    { can=0;
+        cin>>tmp>>tmp1;
+        for(it=node[tmp].begin();it!=node[tmp].end();it++)
+        {   cout<<*it<<endl;
+            for(it1=node[(*it)].begin();it1!=node[(*it)].end();it1++)
+            {cout<<*it1<<endl;
+                   if(*it1==tmp1)
+                   {
+                        can=1;
+                        goto z;
+                   }
+            }
+        }
+        z:
+            cout<<can<<endl;
+    }
+
 return 0;}
