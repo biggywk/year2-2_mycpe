10cb201a6db1eb7ca4cd881b3496e01f5b3402e6
biggywk <big.baby-@hotmail.com>
2018-04-03 01:10:00 +0700

    finish noob jumpgame

--- Probsolve/jumpgame.cpp
+++ Probsolve/jumpgame.cpp
@@ -3,9 +3,13 @@ using namespace std;
 bool visited[20][20];
 int area[20][20];
 int n,jump;
+int mx;
 void go(int a,int b)
 {
-    visited[a][b]=true;
+    if(!visited[a][b])
+    {
+
+        visited[a][b]=true;
         if(area[a][b]+jump>=area[a][b+1])
         {
             if(b<n)
@@ -14,8 +18,20 @@ void go(int a,int b)
         if(area[a][b]+jump>=area[a+1][b])
         {
             if(a<n)
-            go(a+1,b);
+                go(a+1,b);
         }
+        if(area[a][b]+jump>=area[a][b-1])
+        {
+            if(b-1>=0)
+                go(a,b-1);
+        }
+        if(area[a][b]+jump>=area[a-1][b])
+        {
+            if(a-1>=0)
+                go(a-1,b);
+        }
+
+    }
 }
 int main()
 {
@@ -25,10 +41,10 @@ int main()
         for(int j=0; j<n; j++)
             cin>>area[i][j];
     }
-    if(jump>=area[0][0])
-    {
+
+
         go(0,0);
-    }
+
     if(visited[n-1][n-1]==true)
         cout<<"yes";
     else
