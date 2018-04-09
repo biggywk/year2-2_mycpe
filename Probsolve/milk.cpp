a0a54abbe894df3de307f274cdb9e2eba1029fec
biggywk <big.baby-@hotmail.com>
2018-04-09 21:54:08 +0700

    almost

--- Probsolve/milk.cpp
+++ Probsolve/milk.cpp
@@ -8,56 +8,32 @@ using namespace std;
     int tmp;
     int b;
     vector<int> adj[100010];
-    int visited[100010];
-bool bfs(int u)
-{
-
-    visited[u]=true;
-    if(visited[b])
-        return true;
-    for(int i=0;i<adj[u].size();i++)
-    {
-        q.push_back(adj[u][i]);
-        while(!q.empty())
-        {
-            tmp=q.front();
-            q.pop_front();
-            if(!visited[tmp])
-            {
-                bfs(tmp);
-            }
-
-        }
-    }
-}
-void revisited()
-{
-    for(int i=0;i<node;i++)
-    {
-        visited[i]=0;
-    }
-}
+    int parent[100010];
 int main()
 {
     int norder;
     char tmp;
     int a;
+    int asize,bsize;
     //int b;
     cin>>node>>norder;
-    int si;
-    vector<int>::iterator it;
-
+    //int rank[node];
+    vector<int> sub[node];
+    for(int i=0; i<node;i++)
+    {
+        parent[i]=i;
+        sub[i].push_back(i);
+        //rank[i]=0;
+    }
     for(int i=0; i<norder; i++)
     {
         cin>>tmp>>a>>b;
         a--;
         b--;
-        it=adj[a].begin();
         if(tmp=='q')
         {
-            revisited();
-            mixed=bfs(a);
-            if(mixed)
+            cout<<parent[a]<<parent[b]<<endl;
+            if(parent[a]==parent[b])
             {
                 cout<<"yes"<<endl;
             }
@@ -66,14 +42,53 @@ int main()
         }
         else if(tmp=='c')
         {
-            si=adj[a].size();
-            for(int z=0;z<adj[b].size();z++)
+            asize=sub[a].size();
+            bsize=sub[b].size();
+            //cout<<asize<<bsize<<endl;
+            if(asize>=bsize)
             {
-                adj[a].push_back(adj[b][z]);
+                for(int z=0;z<bsize;z++)
+                {
+                    //cout<<sub[b][z]<<endl;
+                    sub[a].push_back(sub[b][z]);
+                    sub[b].push_back(a);
+                }
+                bsize=sub[b].size();
+                for(int z=0;z<bsize;z++)
+                {
+                    parent[sub[b][z]]=a;
+                }
+                for(int z=0;z<sub[a].size();z++)
+                {
+                    parent[sub[a][z]]=a;
+                }
             }
-            for(int z=0;z<si;z++)
+            else
             {
-                adj[b].push_back(adj[a][z]);
+                for(int z=0;z<asize;z++)
+                {
+                    //cout<<sub[a][z]<<endl;
+                    sub[b].push_back(sub[b][z]);
+                    sub[a].push_back(b);
+                   // parent[sub[a][z]]=b;
+                }
+                asize=sub[a].size();
+                for(int z=0;z<asize;z++)
+                {
+                    parent[sub[a][z]]=b;
+                }
+                for(int z=0;z<sub[b].size();z++)
+                {
+                    parent[sub[b][z]]=b;
+                }
+            }
+        }
+        //check sub
+        for(int e=0;e<node;e++)
+        {
+            for(int k=0;k<sub[e].size();k++)
+            {
+                cout<<sub[e][k]<<endl;
             }
         }
     }
