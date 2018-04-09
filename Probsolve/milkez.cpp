608d1106751fdcc08e466fcb276a4e07441937eb
biggywk <big.baby-@hotmail.com>
2018-04-09 22:07:39 +0700

    almost.

---
+++ Probsolve/milkez.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+using namespace std;
+int n,q;
+int arr[100010];
+int find(int i)
+{
+    while(arr[i]!=i)
+    {
+        arr[i]=arr[arr[i]];
+        i=arr[i];
+    }
+    return i;
+}
+void Union(int x,int y)
+{
+    int a=find(x);
+    int b=find(y);
+    if(a<b)
+    {
+        arr[b]=arr[a];
+    }
+    else
+        arr[a]=arr[b];
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n>>q;
+    int x,y;
+    char tmp;
+
+    for(int i=0;i<n;i++)
+    {
+        arr[i]=i;
+    }
+        while(q--)
+        {
+            cin>>tmp>>x>>y;
+            if(tmp=='c')
+                Union(x,y);
+            else if(tmp=='q')
+            {
+                if(find(x)==find(y))
+                {
+                    cout<<"yes"<<"\n";
+                }
+                else
+                    cout<<"no"<<"\n";
+            }
+        }
+
+
+    return 0;
+}
