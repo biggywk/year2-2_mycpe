b8372bace61d92825374a79adafa8315d888fdd6
biggywk <big.baby-@hotmail.com>
2018-04-30 16:56:31 +0700

    No nodejs

--- nodejs/demo_mymodule.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-var dt = require('./myfirstmodule');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end({dt});
-}).listen(8080);
